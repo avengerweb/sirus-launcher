48738446a4db0edbc5f25969c079215a1663c07b
avengerweb <i@avenger-web.com>
2018-10-01 17:28:52 +0300

    [Travis] Mistake with variable name

--- contrib/new-version.sh
+++ contrib/new-version.sh
@@ -1,3 +1,3 @@
 PREVIOUS_APP_VERSION="$(grep -o "<version>.*</version>" pom.xml | head -1 | grep -o "[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*")"
-MINOR=$(echo $APP_VERSION | awk -F '.' '{print $1"."$2"."}')
+MINOR=$(echo $PREVIOUS_APP_VERSION | awk -F '.' '{print $1"."$2"."}')
 echo $MINOR$TRAVIS_BUILD_NUMBER
