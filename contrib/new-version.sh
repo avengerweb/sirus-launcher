276bf6cb52ba9ed1b041fa851e637553e33ed3da
avengerweb <i@avenger-web.com>
2018-10-01 17:17:19 +0300

    Update travis

--- contrib/new-version.sh
+++ contrib/new-version.sh
@@ -1,3 +1,3 @@
-PREVIOUS_APP_VERSION="$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q)"
+PREVIOUS_APP_VERSION="$(grep -o "<version>.*</version>" pom.xml | head -1 | grep -o "[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*")"
 MINOR=$(echo $APP_VERSION | awk -F '.' '{print $1"."$2"."}')
 echo $MINOR$TRAVIS_BUILD_NUMBER
